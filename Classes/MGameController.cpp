934e9d77365a23fe5a4c9806aa539d687c91766b
frank <qinyuanz@qq.com>
2014-09-02 22:39:01 +0800

    save

---
+++ Classes/MGameController.cpp
@@ -0,0 +1,9 @@
+//
+//  MGameController.cpp
+//  EarthWarrior3D
+//
+//  Created by qinyuanz on 14-9-2.
+//
+//
+
+#include "MGameController.h"
---
+++ Classes/MGameController.h
@@ -0,0 +1,14 @@
+//
+//  MGameController.h
+//  EarthWarrior3D
+//
+//  Created by qinyuanz on 14-9-2.
+//
+//
+
+#ifndef EarthWarrior3D_MGameController_h
+#define EarthWarrior3D_MGameController_h
+
+
+
+#endif
